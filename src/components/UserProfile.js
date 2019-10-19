5d638204c13e4373e79bde6712665a99739d0ddb
PJMantoss <joelptoss@gmail.com>
2019-10-19 18:34:05 +0100

    Update UserProfile component

---
+++ src/components/UserProfile.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function UserProfile() {
+    return (
+        <div className="userProfile">
+            <div className="user">
+                <div className="name">PJ Mantoss</div>
+                <div className="image">
+                    <img 
+                        src="https://youknowigotsoul.com/wp-content/uploads/2012/01/Jazz-Dru-Hill-2011-edited.jpg" 
+                        alt="profile" 
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
