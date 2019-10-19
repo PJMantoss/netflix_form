5a855232986f6c1299414919e207469f9c92c01c
PJMantoss <joelptoss@gmail.com>
2019-10-19 19:12:46 +0100

    Add Logo

---
+++ src/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Logo() {
+    return (
+        <div id="logo" className="Logo">
+            <svg version="1.1" width="300" height="81.386726" id="svg3262">
+                <g>
+                    <g></g>
+                </g>
+            </svg>
+        </div>
+    )
+}
