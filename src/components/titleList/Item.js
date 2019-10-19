d8496e46f593dc5da06b5bc2811a1c263b46b39e
PJMantoss <joelptoss@gmail.com>
2019-10-19 17:40:34 +0100

    Update Item and TitleList components

---
+++ src/components/titleList/Item.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ListToggle from './ListToggle';
+
+export default function Item() {
+    return (
+        <div className="Item" style={{backgroundImage: "url(' + this.props.backdrop + ')"}}>
+            <div className="overlay">
+                <div className="title">{this.props.title}</div>
+                <div className="rating">{this.props.score}</div>
+                <div className="plot">{this.props.overview}</div>
+                <ListToggle />
+            </div>
+        </div>
+    )
+}
--- src/components/titleList/TitleList.js
+++ src/components/titleList/TitleList.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Item from './Item';
 
 export class TitleList extends Component {
     constructor(){
@@ -23,12 +24,66 @@ export class TitleList extends Component {
         });
     }
 
+    getDerivedStateFromProps(nextProps){
+        if (nextProps.url !== this.props.url && nextProps.url !== ''){
+            this.setState({
+                mounted: true,
+                url: nextProps.url
+            }, ()=> {
+                this.loadContent();
+            });
+        }
+    }
+
+    componentDidMount(){
+        if (this.props.url !== ''){
+            this.loadContent();
+            tis.setState({
+                mounted: true
+            })
+        }
+    }
+
     render() {
+        const titles = '';
+        if (this.state.data.results){
+            titles = this.state.data.results.map((title, i) => {
+                if (i<5) {
+                    let name = '';
+                    let backDrop = 'https://image.tmdb.org/t/p/original' + title.backdrop_path;
+                    if (!title.name) {
+                        name = title.original_title;
+                    } else {
+                        name = title.name;
+                    }
+
+                    return(
+                        <Item 
+                            key={title.id} 
+                            title={name} 
+                            score={title.vote_average}
+                            overview={title.overview}
+                            backdrop={backDrop} 
+                        />
+                    );
+                } else {
+                    return(
+                        <div key={title.id}></div>
+                    );
+                }
+            });
+        }
+
         return (
-            <div>
-                
+            <div ref="titlecategory" className="TitleList" data-loaded={this.state.mounted}>
+               <div className="Title">
+                   <h1>{this.props.title}</h1>
+                   <div className="titles-wrapper">
+                       {titles}
+                   </div>
+               </div> 
             </div>
-        )
+        );
     }
 }
 
