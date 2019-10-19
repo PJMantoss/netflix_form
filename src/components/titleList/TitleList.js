5272ca04a7b9a594257e3f4a25a9317b497cd4da
PJMantoss <joelptoss@gmail.com>
2019-10-19 11:27:02 +0100

    Create TitleList class

---
+++ src/components/titleList/TitleList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class TitleList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default TitleList
