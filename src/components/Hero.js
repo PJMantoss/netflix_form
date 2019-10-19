d54ac9709380697848084a756e7139ba9c364872
PJMantoss <joelptoss@gmail.com>
2019-10-19 18:13:07 +0100

    Update Hero component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -6,17 +6,17 @@ export default function Hero() {
             <div className="content">
 
                 <img className="logo" src="https://www.returndates.com/backgrounds/narcos.logo.png" alt="narcos background" />
-                <h2></h2>
-                <p></p>
+                <h2>Season 2 Now Available</h2>
+                <p>Narcos is an American crime drama web television series created and produced by Chris Brancato, Carlo Bernard, and Doug Miro. Set and filmed in Colombia, seasons 1 and 2 are based on the story of drug kingpin Pablo Escobar, who became a billionaire through the production and distribution of cocaine.</p>
 
-                <div className="">
+                <div className="button-wrapper">
                     <HeroButton primary={true} text="Watch Now" />
                     <HeroButton primary={false} text="+ My list" />
                 </div>
 
             </div>
 
-            <div className=""></div>
+            <div className="overlay"></div>
 
         </div>
     )
