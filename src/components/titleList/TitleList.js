33ceaa62e65979d1e943496644c69fb7aec09de2
PJMantoss <joelptoss@gmail.com>
2019-10-19 16:42:08 +0100

    Add API key to TitleList

--- src/components/titleList/TitleList.js
+++ src/components/titleList/TitleList.js
@@ -1,6 +1,28 @@
 import React, { Component } from 'react'
 
 export class TitleList extends Component {
+    constructor(){
+        super()
+        this.state = {
+            data: [],
+            mounted: false,
+            apiKey: '6a711b2f9574da2458063937e061f1c3'
+        }
+    }
+
+    loadContent(){
+
+        fetch('https://api.themoviedb.org/3/' + this.props.url + '&api_key=' + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => {
+            this.setState({
+                data: data
+            })
+        }).catch((err) => {
+            console.log('There has been an error');
+        });
+    }
+
     render() {
         return (
             <div>
